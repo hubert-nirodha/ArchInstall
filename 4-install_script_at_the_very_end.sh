699af3889753e4938d39b450fe0c3c207da277c9
hubert-nirodha <100153046+hubert-nirodha@users.noreply.github.com>
2024-02-29 13:21:32 +0100

    Update 4-install_script_at_the_very_end.sh

--- 4-install_script_at_the_very_end.sh
+++ 4-install_script_at_the_very_end.sh
@@ -7,22 +7,8 @@ sleep 2
 sudo pacman -S plasma-browser-integration kinfocenter breeze-plymouth unzip kdeplasma-addons plasma-vault powerdevil plasma-thunderbolt xdg-desktop-portal-kde plasma-disks breeze-gtk xarchiver 
 sleep 2
 
-# Chaotic AUR einrichten (VORSICHT: Experimentelles Repository!)
-
-# Schlüssel für Chaotic AUR hinzufügen:
-pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key 3056513887B78AEB
-
-# Chaotic AUR Paketquellen hinzufügen:
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-
-# Pacman Konfiguration anpassen (Chaotic-AUR Repository ans Ende der pacman.conf Datei hinzufügen):
-echo "[chaotic-aur]" >> /etc/pacman.conf
-echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
-
 #sudo su - nirodha
-sleep 2
+#sleep 2
 # Paket-Manager "yay" installieren:
 #git clone https://aur.archlinux.org/yay.git
 #cd yay
