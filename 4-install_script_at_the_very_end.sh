664bfb5c63332300bd8b08d3eac9efe7e6424187
hubert-nirodha <100153046+hubert-nirodha@users.noreply.github.com>
2024-02-29 13:24:43 +0100

    Update 4-install_script_at_the_very_end.sh

--- 4-install_script_at_the_very_end.sh
+++ 4-install_script_at_the_very_end.sh
@@ -2,33 +2,37 @@
 
 sudo pacman -S --needed pipewire-pulse pipewire-audio 
 sleep 2
-sudo pacman -S linux-docs khotkeys plasma-firewall ttf-liberation linux-tools-meta plymouth-kcm linux-lts-docs kde-gtk-config plasma-workspace-wallpapers yakuake  
+sudo pacman -S --needed linux-docs khotkeys plasma-firewall ttf-liberation linux-tools-meta plymouth-kcm linux-lts-docs kde-gtk-config plasma-workspace-wallpapers yakuake  
 sleep 2
-sudo pacman -S plasma-browser-integration kinfocenter breeze-plymouth unzip kdeplasma-addons plasma-vault powerdevil plasma-thunderbolt xdg-desktop-portal-kde plasma-disks breeze-gtk xarchiver 
+sudo pacman -S --needed plasma-browser-integration kinfocenter breeze-plymouth unzip kdeplasma-addons plasma-vault powerdevil plasma-thunderbolt xdg-desktop-portal-kde plasma-disks breeze-gtk xarchiver 
 sleep 2
 
 #sudo su - nirodha
 #sleep 2
 # Paket-Manager "yay" installieren:
-#git clone https://aur.archlinux.org/yay.git
-#cd yay
-#makepkg -si
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
 
 # Hinweis: Die Verwendung von "yay" kann potenziell unsicher sein, da es Pakete außerhalb der offiziellen Repositories installiert. Verwenden Sie es mit Vorsicht!
 
 #sleep 5
 
 # Update the databases, repositories and the whole system
-pacman -Syu
+sudo pacman -Syu && yay
+
+
+# Google Chrome installieren
+yay -S google-chrome
 
 # Microsoft Fonts installieren:
-#yay -s ttf-ms-fonts
-
-#yay -S mesa-git
+yay -S ttf-ms-fonts
 
 # Timeshift für System-Snapshots installieren:
-#yay -S timeshift
+yay -S timeshift
 
 # qt5-webkit für bestimmte Anwendungen:
-#yay -S qt5-webkit
+yay -S qt5-webkit
 
